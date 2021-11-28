d7e28069d9060d2b6cea76db957a00e77201515e
unknown <admin@SP4-L44-C13.first.local>
2021-11-28 09:57:14 +0500

    'first'

---
+++ pages/index.vue
@@ -0,0 +1,132 @@
+
+<template>
+  <v-main class="welcome">
+   <div class="container">
+      <div class="welcome-wrapper">
+            <div class="welcome-content animate__animated animate__backInLeft">
+              <h1 class="welcome-title ">
+                Виртуальные <br />
+                Открытки
+              </h1>
+              <p class="welcome-text">
+                Здесь вы найдёте уникальные виртуальные открытки<br />
+                для друзей и знакомых<br />
+                Создавайте собственные вирутальные открытки<br />
+                Делитесь ими <br />
+              </p>
+              <button class="btn welcome-btn">Генерировать</button>
+            </div>
+            <div class="welcome-Aframe animate__animated animate__backInRight"></div>   
+        </div>
+   </div>
+  </v-main>
+</template>
+
+
+<style lang="scss" >
+@media (min-width: 1904px){
+  .container {
+      max-width: none;
+  }
+}
+@media (min-width: 1264px){
+  .container {
+      max-width: none;
+  }
+}
+@media (max-width: 1342px){
+  .container {
+      max-width: none;
+  }
+  .welcome {
+    &-content{
+      max-width: 50%;
+    }
+    &-Aframe{
+      width: 400px !important;
+    }
+    &-title{
+      font-size: 60px !important;
+    }
+    &-text{
+      font-size: 20px !important;
+    }
+  }
+
+}
+@media (max-width: 873px){
+  // .v-main__wrap{
+  //   height: 200vh;
+  // }
+}
+.welcome {
+  .v-main__wrap{
+    display: flex;
+    align-items: center;
+  }
+  background: url("./assets/image/welcome-back.png") no-repeat;
+  background-size: cover;
+  height: 100vh;
+  &-wrapper {
+    color: white;
+    width: 100%;
+    justify-content: space-around;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  
+  &-content{
+    border-radius: 25px !important;
+    min-height: 458px !important;
+    padding: 20px 40px 20px 40px;
+  }
+
+  &-btn {
+    padding: 27px 65px;
+    font-size: 1.5em;
+    line-height: 28px;
+    transition: all ease-in-out .2s;
+    &:hover{
+        transform: translateY(-10%);
+    }
+  }
+  &-title {
+    font-size: 72px;
+    line-height: 84px;
+    font-weight: bold;
+    margin-bottom: 63px;
+    color: #fbfbfb;
+  }
+  &-text {
+    // font-weight: 500;
+    margin-bottom: 87px;
+    font-size: 24px;
+    line-height: 28px;
+  }
+  &-Aframe{
+    width: 624px;
+    min-width: 400px;
+    min-height: 458px;
+    height: 715px;
+    background-color: white;
+    border-radius: 50px;
+  }
+}
+</style>
+
+<script>
+import 'animate.css';
+
+export default {
+  layout: "empty",
+  mounted() {
+    
+  },
+  data() {
+    return {};
+  },
+};
+
+
+</script>
---
+++ pages/signin.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="signin">
+      <div class="signin-content">
+        <h2 class="sign-title">Вход</h2>
+        <div class="sign-inputs">
+          <div class="sign-input">
+            <img src="../assets/image/acount.svg" alt="" />
+            <input
+              type="text"
+              placeholder="E-mail"
+            />
+          </div>
+          <div class="sign-input">
+            <img src="../assets/image/lock.svg" alt="" /><input
+              type="password"
+              placeholder="Пароль"
+            />
+          </div>
+        </div>
+        <div class="sign-buttons">
+          <button class="sign-button btn">Войти</button>
+          <NuxtLink to="/signup" class="sign-button_signup"
+            >Зарегестрироваться</NuxtLink
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "empty",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+.signin {
+  background: url("./assets/image/signin-back.png") no-repeat;
+  background-size: cover;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  &-content {
+    padding: 64px 120px;
+    width: max-content;
+    background-color: white;
+    border-radius: 50px;
+    margin: 0 auto;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    transform: translateY(-50%);
+  }
+  .sign-input {
+    border-bottom: 1px solid #f6851b;
+    display: flex;
+    align-items: center;
+    padding: 10px 0;
+    input{
+        padding-left: 12px;
+        &:focus{
+            border: none;
+            outline: none;
+        }
+    }
+   
+    
+    &:nth-child(1) {
+      margin-bottom: 42px;
+    }
+  }
+}
+.sign {
+  &-title {
+    font-size: 64px;
+    text-align: center;
+    line-height: 75px;
+    margin-bottom: 91px;
+  }
+  &-inputs {
+    margin-bottom: 88px;
+  }
+  &-button {
+    margin-bottom: 17px;
+    &_signup {
+      color: #333333;
+    }
+    &s {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+    padding: 16px 80px;
+  }
+}
+</style>
